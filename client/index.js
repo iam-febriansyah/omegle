302a3a5e69f163f3376debf5e12dd60a7819e135
Febriansyah <febriansyah032@gmail.com>
2024-05-09 06:21:10 +0700

    server

--- client/index.js
+++ client/index.js
@@ -13,133 +13,131 @@ let roomid;
 
 // starts media capture
 function start() {
-    navigator.mediaDevices
-        .getUserMedia({ audio: true, video: true })
-        .then((stream) => {
-            if (peer) {
-                myVideo.srcObject = stream;
-                stream
-                    .getTracks()
-                    .forEach((track) => peer.addTrack(track, stream));
+  navigator.mediaDevices
+    .getUserMedia({ audio: true, video: true })
+    .then((stream) => {
+      if (peer) {
+        myVideo.srcObject = stream;
+        stream.getTracks().forEach((track) => peer.addTrack(track, stream));
 
-                peer.ontrack = (e) => {
-                    strangerVideo.srcObject = e.streams[0];
-                    strangerVideo.play();
-                };
-            }
-        })
-        .catch((ex) => {
-            console.log(ex);
-        });
+        peer.ontrack = (e) => {
+          strangerVideo.srcObject = e.streams[0];
+          strangerVideo.play();
+        };
+      }
+    })
+    .catch((ex) => {
+      console.log(ex);
+    });
 }
 
 // connect ot server
-const socket = io("http://localhost:8000");
+const socket = io("https://omegle-backend.febriansyah.dev");
 
 // disconnectin event
 socket.on("disconnected", () => {
-    location.href = `/?disconnect`;
+  location.href = `/?disconnect`;
 });
 
 /// --------- Web rtc related ---------
 
 // Start
 socket.emit("start", (person) => {
-    type = person;
+  type = person;
 });
 
 // Get remote socket
 
 socket.on("remote-socket", (id) => {
-    remoteSocket = id;
+  remoteSocket = id;
 
-    // hide the spinner
-    document.querySelector(".modal").style.display = "none";
+  // hide the spinner
+  document.querySelector(".modal").style.display = "none";
 
-    // create a peer conncection
-    peer = new RTCPeerConnection();
+  // create a peer conncection
+  peer = new RTCPeerConnection();
 
-    // on negociation needed
-    peer.onnegotiationneeded = async (e) => {
-        webrtc();
-    };
+  // on negociation needed
+  peer.onnegotiationneeded = async (e) => {
+    webrtc();
+  };
 
-    // send ice candidates to remotesocket
-    peer.onicecandidate = (e) => {
-        socket.emit("ice:send", { candidate: e.candidate, to: remoteSocket });
-    };
+  // send ice candidates to remotesocket
+  peer.onicecandidate = (e) => {
+    socket.emit("ice:send", { candidate: e.candidate, to: remoteSocket });
+  };
 
-    // start media capture
-    start();
+  // start media capture
+  start();
 });
 
 // creates offer if 'type' = p1
 async function webrtc() {
-    if (type == "p1") {
-        const offer = await peer.createOffer();
-        await peer.setLocalDescription(offer);
-        socket.emit("sdp:send", { sdp: peer.localDescription });
-    }
+  if (type == "p1") {
+    const offer = await peer.createOffer();
+    await peer.setLocalDescription(offer);
+    socket.emit("sdp:send", { sdp: peer.localDescription });
+  }
 }
 
 // recive sdp sent by remote socket
 socket.on("sdp:reply", async ({ sdp, from }) => {
-    // set remote description
-    await peer.setRemoteDescription(new RTCSessionDescription(sdp));
+  // set remote description
+  await peer.setRemoteDescription(new RTCSessionDescription(sdp));
 
-    // if type == p2, create answer
-    if (type == "p2") {
-        const ans = await peer.createAnswer();
-        await peer.setLocalDescription(ans);
-        socket.emit("sdp:send", { sdp: peer.localDescription });
-    }
+  // if type == p2, create answer
+  if (type == "p2") {
+    const ans = await peer.createAnswer();
+    await peer.setLocalDescription(ans);
+    socket.emit("sdp:send", { sdp: peer.localDescription });
+  }
 });
 
 // recive ice-candidate form remote socket
 socket.on("ice:reply", async ({ candidate, from }) => {
-    await peer.addIceCandidate(candidate);
+  await peer.addIceCandidate(candidate);
 });
 
 /// ----------- Handel Messages Logic -----------
 
 // get room id
 socket.on("roomid", (id) => {
-    roomid = id;
+  roomid = id;
 });
 
 // handel send button click
 button.onclick = (e) => {
-    send(e);
+  send(e);
 };
 
 input.addEventListener("keypress", (e) => {
+  console.log(e);
+  if (e.key == "Enter" || e.charCode == 13) {
     console.log(e);
-    if (e.key == "Enter" || e.charCode == 13) {
-        console.log(e);
-        send(e);
-    }
+    send(e);
+  }
 });
 
 function send(e) {
-    // get input and emit
-    let input = document.querySelector("input").value;
-    socket.emit("send-message", input, type, roomid);
+  // get input and emit
+  let input = document.querySelector("input").value;
+  socket.emit("send-message", input, type, roomid);
 
-    // set input in local message box as 'YOU'
-    let msghtml = `<div class="msg"><b>You: </b> <span id='msg'>${input}</span></div>`;
-    document.querySelector(".chat-holder .wrapper").innerHTML += msghtml;
+  // set input in local message box as 'YOU'
+  let msghtml = `<div class="msg"><b>You: </b> <span id='msg'>${input}</span></div>`;
+  document.querySelector(".chat-holder .wrapper").innerHTML += msghtml;
 
-    // clear input
-    document.querySelector("input").value = "";
+  // clear input
+  document.querySelector("input").value = "";
 }
 
 // on get message
 socket.on("get-message", (input, type) => {
-    // set recived message from server in chat box
-    let msghtml = `
+  // set recived message from server in chat box
+  let msghtml = `
   <div class="msg">
   <b>Stranger: </b> <span id='msg'>${input}</span>
   </div>
   `;
-    document.querySelector(".chat-holder .wrapper").innerHTML += msghtml;
+  document.querySelector(".chat-holder .wrapper").innerHTML += msghtml;
 });
